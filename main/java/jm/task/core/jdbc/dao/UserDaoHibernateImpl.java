8f0252d56cf69ae580e2c6eb211ec6f662ee7823
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-01-12 20:20:45 +0300

    Commit-1 12.01.21 Task 1.1.4

--- main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -5,14 +5,20 @@ import jm.task.core.jdbc.util.Util;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.query.NativeQuery;
 import org.hibernate.query.Query;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private static UserDaoHibernateImpl userDaoHibernate;
+
     public UserDaoHibernateImpl() {
+        this.userDaoHibernate = UserDaoHibernateImpl.getUserDaoHibernate();
+    }
+
+    public static UserDaoHibernateImpl getUserDaoHibernate() {
+        return userDaoHibernate;
     }
 
     @Override
@@ -21,7 +27,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
         try (Session session = Util.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
-           TypedQuery sqlQuery = session.createSQLQuery("CREATE TABLE IF NOT EXISTS USER  ("
+            TypedQuery sqlQuery = session.createSQLQuery("CREATE TABLE IF NOT EXISTS USER  ("
                     + "Id INT (6) PRIMARY KEY NOT NULL AUTO_INCREMENT, "
                     + "Name VARCHAR (80) NOT NULL, "
                     + "LastName VARCHAR (80) NOT NULL, "
@@ -73,7 +79,8 @@ public class UserDaoHibernateImpl implements UserDao {
 
         try (Session session = Util.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
-            session.delete(session.get(User.class, id));
+            Query query = session.createQuery("delete User where id = :deleted_id");
+            query.setParameter("deleted_id", id).executeUpdate();
             transaction.commit();
         } catch (HibernateException e) {
             if (transaction != null) {
@@ -89,8 +96,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
         try (Session session = Util.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
-            Query query = session.createSQLQuery("SELECT * FROM USER");
-             ((NativeQuery) query).addEntity(User.class);
+            Query query = session.createQuery("from User");
             userList = query.list();
             transaction.commit();
         } catch (HibernateException e) {
